10d38ce4ec157c631c0aee52b7571ae373e6cb92
sergio noriega cantillo <sergionoriegacantillo@gmail.com>
2024-07-10 17:27:08 +0200

    I added the routes of the website

--- src/pages/App/index.jsx
+++ src/pages/App/index.jsx
@@ -1,13 +1,38 @@
-
+import { useRoutes, BrowserRouter } from 'react-router-dom'
+import { Home } from '../Home'
+import { Myaccount } from '../Myaccount'
+import { Myorder } from '../Myorder'
+import { Myorders } from '../Myorders'
+import { NotFound } from '../NotFound'
+import { Signin } from '../Signin'
 import './App.css'
 
-function App() {
+
+
+const AppRoutes = () =>{
+  
+  let routes = useRoutes([
+    { path: '/', element: <Home /> },
+    { path: '/myaccount', element: <Myaccount /> },
+    { path: '/myorder', element: <Myorder /> },
+    { path: '/myorders', element: <Myorders /> },
+    { path: '/signin', element: <Signin /> },
+    {path: '/*',element: <NotFound />},
+  ]);
+
+
+  return routes;
+}
+
+const App  = () => { 
+
 
   return (
     <>
-      <div className='bg-blue-600'>
-        App page
-      </div>
+
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
     </>
   )
 }
